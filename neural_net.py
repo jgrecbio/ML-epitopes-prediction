20a8584ff86fabfe78f9a82e40f86291154bfebc
jgrec <jgrec.bio@gmail.com>
2018-01-14 15:02:20 +0100

    Removal of input shape if pre_model is given as parameters

--- neural_net.py
+++ neural_net.py
@@ -4,12 +4,12 @@ from keras.layers import Dense
 from keras.losses import mean_squared_error
 
 
-def dense_model(input_shape,
-                nb_units: Union[List[int], int],
+def dense_model(nb_units: Union[List[int], int],
                 activations: Union[List, object],
                 optimizer,
                 loss=mean_squared_error,
-                model=None,
+                input_shape=None,
+                pre_model=None,
                 *args, **kwargs
                 ) -> Sequential:
     """
@@ -18,11 +18,11 @@ def dense_model(input_shape,
     :param nb_units: number of neurons per layer or list of number of neurons per layer
     :param activations: activation function to use or list of activation to use per layer
     :param optimizer: keras optimizer to use
-    :param: model: a keras model to iterate on
+    :param: pre_model: a keras model to iterate on
     :param loss: loss function
     :param args: args to pass to the optimizer
     :param kwargs: kwargs to pass to the optimizer
-    :return:
+    :return: keras Sequential model
     """
 
     if isinstance(nb_units, List) and isinstance(activations, list):
@@ -41,10 +41,13 @@ def dense_model(input_shape,
     else:
         acts = activations
 
-    model = Sequential() if not model else model
-    model.add(Dense(input_shape=input_shape, units=units[0], activation=acts[0]))
+    model = Sequential() if not pre_model else pre_model
+    if input_shape and not pre_model:
+        model.add(Dense(input_shape=input_shape, units=units[0], activation=acts[0]))
+    else:
+        model.add(Dense(units=units[0], activation=acts[0]))
 
-    for nb_neurons, activation in zip(units, acts):
+    for nb_neurons, activation in zip(units[1:], acts[1:]):
         model.add(Dense(units=nb_neurons, activation=activation))
 
     model.compile(optimizer=optimizer(*args, **kwargs), loss=loss)
