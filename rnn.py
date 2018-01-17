34b0c128ed2e04aed09feee9951560725b2cb3e6
jgrec <jgrec.bio@gmail.com>
2018-01-17 06:17:59 +0100

    Transformation of RNN model into Stacked RNN
    + add comments + docstring for rnn_model

--- rnn.py
+++ rnn.py
@@ -7,17 +7,37 @@ from neural_net import dense_model
 def rnn_model(input_dim: int=20,
               output_dim: int=50,
               input_length: int=9,
+              nb_rnn_neurons: List[int]=None,
               dense_nb_neurons: Optional[List[int]]=None,
               dense_activations: Optional[List]=None,
               rnn_cell_type=GRU, pre_model=None, dropout: int=0.5) -> Sequential:
+    """
+    Stacked RNN model
+    :param input_dim: length of the vocabulary of vectors
+    :param output_dim: nb of components of aa vectors
+    :param input_length: length of tokenized vectors
+    :param nb_rnn_neurons: nb of neurons per RNN layers
+    :param dense_nb_neurons: nb of neurons per dense layer
+    :param dense_activations: type of activation per dense layer
+    :param rnn_cell_type: type RNN
+    :param pre_model: keras Sequential
+    :param dropout: rate of dropout for LSTM regularization
+    :return:
+    """
 
     model = Sequential() if not pre_model else pre_model
 
+    # Embedding layer
     model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=input_length))
-    model.add(Bidirectional(rnn_cell_type(32, return_sequences=True)))
-    model.add(Dropout(dropout))
-    model.add(Flatten())
 
+    # Stacked RNN
+    rnn_neurons = nb_rnn_neurons if nb_rnn_neurons else [32]
+    for nb_neurons in rnn_neurons:
+        model.add(Bidirectional(rnn_cell_type(nb_neurons, return_sequences=True)))
+        model.add(Dropout(dropout))
+
+    # Dens model
+    model.add(Flatten())
     if dense_activations and dense_nb_neurons:
         return dense_model(nb_units=dense_nb_neurons, activations=dense_activations, pre_model=model)
 
