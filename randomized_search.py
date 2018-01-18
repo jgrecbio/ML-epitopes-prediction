b88d02a6eb604402f2917992de9291002d713c62
jgrec <jgrec.bio@gmail.com>
2018-01-18 13:16:29 +0100

    Add name to neural nets in randomized
    use of names for logs in tensorboard

--- randomized_search.py
+++ randomized_search.py
@@ -29,25 +29,28 @@ def to_regressor(model_fn: Callable, **kwargs) -> KerasRegressor:
 
 
 # fully connected networks
-dense1 = dense_model([40, 40, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,))
-dense2 = dense_model([80, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,))
-dense3 = dense_model([120, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,))
-dense4 = dense_model([80, 80, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,))
+dense1 = dense_model([40, 40, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,), name="dense1")
+dense2 = dense_model([80, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,), name="dense2")
+dense3 = dense_model([120, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,), name="dense3")
+dense4 = dense_model([80, 80, 80, 1], [relu, relu, linear], dropout_reg=0.3, input_shape=(180,), name="dense4")
 
 # cnn networks
 reshape_model = Sequential(layers=[Reshape(input_shape=(180,), target_shape=(180, 1))])
 
 cnn1 = cnn_model(conv_layout=conv_operation(Conv1D, 32, 4, 1, "valid", relu, MaxPooling1D, 2),
-                 pre_model=reshape_model, dense_nb_neurons=[40, 1], dense_activations=[relu, linear])
+                 pre_model=reshape_model, dense_nb_neurons=[40, 1], dense_activations=[relu, linear],
+                 name="cnn1")
 cnn2 = cnn_model(conv_layout=conv_operation(Conv1D, 64, 2, 1, "valid", relu, MaxPooling1D, 2),
-                 pre_model=reshape_model, dense_nb_neurons=[120, 1], dense_activations=[relu, linear])
+                 pre_model=reshape_model, dense_nb_neurons=[120, 1], dense_activations=[relu, linear],
+                 name="cnn2")
 cnn3 = cnn_model(conv_layout=conv_operation(Conv1D, 64, 4, 1, "valid", relu, MaxPooling1D, 4),
-                 pre_model=reshape_model, dense_nb_neurons=[120, 1], dense_activations=[relu, linear])
+                 pre_model=reshape_model, dense_nb_neurons=[120, 1], dense_activations=[relu, linear],
+                 name="cnn3")
 
-# rnn model
-rnn1 = rnn_model(nb_rnn_neurons=[32, 32], dense_nb_neurons=[40, 1], dense_activations=[relu, linear])
-rnn2 = rnn_model(nb_rnn_neurons=[64, 64], dense_nb_neurons=[40, 1], dense_activations=[relu, linear])
-rnn3 = rnn_model(nb_rnn_neurons=[128, 64], dense_nb_neurons=[40, 1], dense_activations=[relu, linear])
+# rnn models
+rnn1 = rnn_model(nb_rnn_neurons=[32, 32], dense_nb_neurons=[40, 1], dense_activations=[relu, linear], name="rnn1")
+rnn2 = rnn_model(nb_rnn_neurons=[64, 64], dense_nb_neurons=[40, 1], dense_activations=[relu, linear], name="rnn2")
+rnn3 = rnn_model(nb_rnn_neurons=[128, 64], dense_nb_neurons=[40, 1], dense_activations=[relu, linear], name="rnn3")
 
 nns = [dense1, dense2, dense3, dense4,
        cnn1, cnn2, cnn3,
@@ -57,8 +60,9 @@ nns = [dense1, dense2, dense3, dense4,
 def grid_search(x, y, categories, nets):
     scorer = make_scorer(mean_squared_error, greater_is_better=False, )
 
-    for i, net in enumerate(nets):
-        cb = TensorBoard(log_dir="./Graph_nn_{}".format(i), histogram_freq=0, write_graph=True, write_images=True)
+    for net in nets:
+        cb = TensorBoard(log_dir="./Graph_nn_{}".format(net.name),
+                         histogram_freq=0, write_graph=True, write_images=True)
         inner_cv = StratifiedKFold(categories)
         best_model = GridSearchCV(estimator=net, scoring=scorer, cv=inner_cv, fit_params={"callbacks": [cb]},
                                   param_grid={
